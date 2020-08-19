5a5fdb54fa0b984c388c36d39cf3827e1571a138
firosstuart <k934277599@gmail.com>
2020-08-19 17:36:15 +0900

    Design changes

--- js/main.js
+++ js/main.js
@@ -7,24 +7,4 @@
             checkBox.checked = false;
         })
     })
-
-    //Get the button:
-    const button1 = document.getElementById("myBtn");
-
-    // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function() {scrollFunction()};
-
-    function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            button1.style.display = "block";
-        } else {
-            button1.style.display = "none";
-        }
-    }
-
-    // When the user clicks on the button, scroll to the top of the document
-    function topFunction() {
-        document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-    }
 }
